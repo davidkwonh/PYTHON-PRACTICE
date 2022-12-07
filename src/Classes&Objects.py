e737fca5552feb332d4e44212ee0924b5ad0c358
David H Kwon <David.Kwon02@student.csulb.edu>
2022-12-06 16:38:24 -0800

    Static and Instance Attributes of Classes
    Finding methods to declare both static and Instance attributes of a class

--- src/Classes&Objects.py
+++ src/Classes&Objects.py
@@ -1,13 +1,40 @@
 # #   classes practice using animals
-#
-# class Dog: # classes usually start with capital letters
-#     def __init__(self, name): # initialization, whenever class Dog is called, this initialization function is calle
-#         self.name = name
-#         self.legs = 4
-#
-#     def speak(self): # declaring self in the parameter gives access to all other functions and variables in class
-#         print(self.name + " says: Bark!")
-#
+import math
+class Dog: # classes usually start with capital letters
+
+    _legs = 4   #   _ signifies variables that are static and don't require change
+                #   static variables because they are unchanging in each instance, constants and fundamental business logic
+    def __init__(self, name): # initialization, whenever class Dog is called, this initialization function is calle
+        #   instance attributes declared because they are the attributes of every instance of the class
+        self.name = name
+
+    def getLegs(self):
+        return self._legs
+
+    def speak(self): # declaring self in the parameter gives access to all other functions and variables in class
+        print(self.name + " says: Bark!")
+
+
+
+
+myDog = Dog('Rover')
+myDog._legs=3
+print(myDog.name)
+print(myDog.getLegs())
+print(Dog._legs)
+
+
+
+
+
+
+
+
+
+
+
+
+
 # mydog = Dog("muffy") #function and variables are lowercase, class names are capitalized
 # wholetthedogsout = Dog("boots")
 #
@@ -49,3 +76,51 @@ rando.age = age
 rando.weight = weight
 rando.height = height
 print(name + " You're BMI is: " + str(rando.bmi()))     #   str() necessary because bmi() is of type float
+
+#   static and instance attributes
+
+class Orientation:
+    pi = 3.14
+
+    def __init__(self, x_pos, y_pos, degrees):
+        self.x_pos = x_pos
+        self.y_pos = y_pos
+        #   Requires the declaration of Orientation class instead of self because static function rather than an instance function
+        self.x_dir, self.y_dir = Orientation.getUnitVectorFromDegrees(degrees)
+
+#   STATIC FUNCTION HENCE NO self. is necessary
+
+    def getUnitVectorFromDegrees(degrees):
+        radians = (degrees / 180) * Orientation.pi
+        return math.sin(radians), -math.cos(radians)
+
+    def getNextPos(self):
+        return self.x_pos + self.x_dir, self.y_pos + self.y_dir
+
+
+myOrientation = Orientation(5, 5, 75)
+myOrientation.getNextPos()
+
+
+class Orientation:
+    pi = 3.14
+
+    def __init__(self, x_pos, y_pos, degrees):
+        self.x_pos = x_pos
+        self.y_pos = y_pos
+        self.x_dir, self.y_dir = self.getUnitVectorFromDegrees(degrees)
+
+    # One can use the static decorator to declare to python that this is a static var rendering the self.getUnitVectorFromDegrees as a static function
+
+    @staticmethod
+    def getUnitVectorFromDegrees(degrees):
+        #   using static variable pi of Orientation class
+        radians = (degrees / 180) * Orientation.pi
+        return math.sin(radians), -math.cos(radians)
+
+    def getNextPos(self):
+        return self.x_pos + self.x_dir, self.y_pos + self.y_dir
+
+
+myOrientation = Orientation(5, 5, 75)
+myOrientation.getNextPos()
